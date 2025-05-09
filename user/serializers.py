ee2f9a3d79d5a70bf012663c994b2279ba389a51
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-05-09 17:46:33 +0500

    /

--- user/serializers.py
+++ user/serializers.py
@@ -7,6 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = '__all__'
 
+    def create(self, validated_data):
+        return Profile.objects.create_user(**validated_data)
+
     # def get_freelancer_skills(self, obj):
     #     if obj.is_freelancer:
     #         skills = FreelancerSkill.objects.filter(user=obj)
